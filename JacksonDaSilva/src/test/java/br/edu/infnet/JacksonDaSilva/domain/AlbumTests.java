46e28c8ddfbb053f6f56d5d853a302e2f79f966f
Jackson Silva <jacksonslv36@gmail.com>
2024-03-16 22:18:26 -0300

    Projeto coompleto.

--- JacksonDaSilva/src/test/java/br/edu/infnet/JacksonDaSilva/domain/AlbumTests.java
+++ JacksonDaSilva/src/test/java/br/edu/infnet/JacksonDaSilva/domain/AlbumTests.java
@@ -1,92 +1,83 @@
-//package br.edu.infnet.JacksonDaSilva.domain;
-//
-//import br.edu.infnet.JacksonDaSilva.model.domain.Album;
-//import br.edu.infnet.JacksonDaSilva.model.domain.Faixa;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.time.Duration;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//public class AlbumTests {
-//    private Album album = new Album();
-//
-//    @Test
-//    void albumSemArtista() {
-//        String resultado = "falha";
-//        try {
-//            album.setArtista("");
-//        } catch (IllegalArgumentException exception) {
-//            resultado = "sucesso";
-//        } finally {
-//            assertEquals("sucesso", resultado);
-//        }
-//    }
-//
-//    @Test
-//    void albumSemTitulo() {
-//        String resultado = "falha";
-//        try {
-//            album.setTitulo("");
-//        } catch (IllegalArgumentException exception) {
-//            resultado = "sucesso";
-//        } finally {
-//            assertEquals("sucesso", resultado);
-//        }
-//    }
-//
-//    @Test
-//    void albumSemGenero() {
-//        String resultado = "falha";
-//        try {
-//            album.setGenero("");
-//        } catch (IllegalArgumentException exception) {
-//            resultado = "sucesso";
-//        } finally {
-//            assertEquals("sucesso", resultado);
-//        }
-//    }
-//
-//    @Test
-//    void albumSemFaixas() {
-//        assertEquals("", album.getStringFaixas());
-//        assertEquals(0, album.getFaixas().size());
-//        assertNull(album.getDuracao());
-//    }
-//
-//    @Test
-//    void albumVazio() {
-//        assertEquals("""
-//                Artista: Desconhecido, Título: Não informado, Gênero: Alternativo, Número de faixas: 0
-//                Faixas: Álbum vazio.
-//                """, album.toString());
-//    }
-//
-//    @Test
-//    void albumCompleto() {
-//        album = new Album("Pentakill", "iii: Lost Chapter", "Rock", "2021");
-//        Faixa lostChapter = new Faixa("Lost Chapter", Duration.ofMinutes(3).plusSeconds(49));
-//        Faixa predator = new Faixa("Predator", Duration.ofMinutes(4).plusSeconds(19));
-//        Faixa edgeOfNight = new Faixa("Edge of Night", Duration.ofMinutes(4).plusSeconds(48));
-//        Faixa gatheringStorm = new Faixa("Gathering Storm", Duration.ofMinutes(4).plusSeconds(37));
-//        Faixa conqueror = new Faixa("Conqueror", Duration.ofMinutes(4).plusSeconds(10));
-//
-//        album.addFaixas(lostChapter);
-//        album.addFaixas(predator);
-//        album.addFaixas(edgeOfNight);
-//        album.addFaixas(gatheringStorm);
-//        album.addFaixas(conqueror);
-//
-//        assertEquals("""
-//                Artista: Pentakill, Título: iii: Lost Chapter, Gênero: Rock, Número de faixas: 5, Duração: 21:43
-//                Faixas:\s
-//                Título: Lost Chapter, Duração: 03:49
-//                Título: Predator, Duração: 04:19
-//                Título: Edge of Night, Duração: 04:48
-//                Título: Gathering Storm, Duração: 04:37
-//                Título: Conqueror, Duração: 04:10
-//                """, album.toString());
-//    }
-//}
+package br.edu.infnet.JacksonDaSilva.domain;
+
+import br.edu.infnet.JacksonDaSilva.model.domain.Album;
+import br.edu.infnet.JacksonDaSilva.model.domain.Artista;
+import br.edu.infnet.JacksonDaSilva.model.domain.Faixa;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class AlbumTests {
+    private Album album = new Album();
+
+    @Test
+    void albumSemArtista() {
+        String resultado = "falha";
+        try {
+            album.setArtista(null);
+        } catch (IllegalArgumentException exception) {
+            resultado = "sucesso";
+        } finally {
+            assertEquals("sucesso", resultado);
+        }
+    }
+
+    @Test
+    void albumSemTitulo() {
+        String resultado = "falha";
+        try {
+            album.setTitulo("");
+        } catch (IllegalArgumentException exception) {
+            resultado = "sucesso";
+        } finally {
+            assertEquals("sucesso", resultado);
+        }
+    }
+
+    @Test
+    void albumSemFaixas() {
+        assertEquals("", album.getStringFaixas());
+        assertEquals(0, album.getFaixas().size());
+        assertNull(album.getDuracao());
+    }
+
+    @Test
+    void albumVazio() {
+        assertEquals("""
+                
+                Id: null, Artista: Anônimo, Título: Não informado, Número de faixas: 0
+                Faixas: Álbum vazio.
+                """, album.toString());
+    }
+
+    @Test
+    void albumCompleto() {
+        album = new Album("iii: Lost Chapter", new Artista("Pentakill"), "3ls");
+        Faixa lostChapter = new Faixa("Lost Chapter", Duration.ofMinutes(3).plusSeconds(49));
+        Faixa predator = new Faixa("Predator", Duration.ofMinutes(4).plusSeconds(19));
+        Faixa edgeOfNight = new Faixa("Edge of Night", Duration.ofMinutes(4).plusSeconds(48));
+        Faixa gatheringStorm = new Faixa("Gathering Storm", Duration.ofMinutes(4).plusSeconds(37));
+        Faixa conqueror = new Faixa("Conqueror", Duration.ofMinutes(4).plusSeconds(10));
+
+        album.addFaixas(lostChapter);
+        album.addFaixas(predator);
+        album.addFaixas(edgeOfNight);
+        album.addFaixas(gatheringStorm);
+        album.addFaixas(conqueror);
+
+        assertEquals("""
+                
+                Id: 3ls, Artista: Pentakill, Título: iii: Lost Chapter, Número de faixas: 5, Duração: 21:43
+                Faixas:\s
+                Id: null, Título: Lost Chapter, Duração: 03:49
+                Id: null, Título: Predator, Duração: 04:19
+                Id: null, Título: Edge of Night, Duração: 04:48
+                Id: null, Título: Gathering Storm, Duração: 04:37
+                Id: null, Título: Conqueror, Duração: 04:10
+                """, album.toString());
+    }
+}
--- JacksonDaSilva/src/test/java/br/edu/infnet/JacksonDaSilva/service/ArtistaServiceTests.java
+++ JacksonDaSilva/src/test/java/br/edu/infnet/JacksonDaSilva/service/ArtistaServiceTests.java
@@ -1,51 +1,57 @@
-//package br.edu.infnet.JacksonDaSilva.service;
-//
-//import br.edu.infnet.JacksonDaSilva.model.domain.Album;
-//import br.edu.infnet.JacksonDaSilva.model.domain.Artista;
-//import br.edu.infnet.JacksonDaSilva.model.service.ArtistaService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//public class ArtistaServiceTests {
-//
-//	@Autowired
-//	private ArtistaService artistaService;
-//
-//	private Artista artista;
-//
-//	private final String NOME = "Mitski";
-//
-//	@BeforeEach
-//	void setUp() {
-//		artista = new Artista(NOME);
-//	}
-//
-//	@Test
-//	void inclusao() {
-//
-//		artistaService.incluir(artista);
-//
-//		assertEquals(artista, artistaService.obter(NOME));
-//	}
-//
-//	@Test
-//	void exclusao() {
-//		inclusao();
-//
-//		artistaService.excluir(NOME);
-//
-//        assertNull(artistaService.obter(NOME));
-//	}
-//
-//	@Test
-//	void recuperacaoTotal() {
-//		inclusao();
-//
-//		assertTrue(artistaService.obterLista().contains(artista));
-//	}
-//}
+package br.edu.infnet.JacksonDaSilva.service;
+
+import br.edu.infnet.JacksonDaSilva.model.domain.Artista;
+import br.edu.infnet.JacksonDaSilva.model.service.ArtistaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ArtistaServiceTests {
+
+	@Autowired
+	private ArtistaService artistaService;
+
+	private Artista artista;
+
+	private final String NOME = "Mitski";
+
+    private final String ID = "mts";
+
+	@BeforeEach
+	void setUp() {
+		artista = new Artista(NOME, ID);
+	}
+
+	@Test
+	void inclusao() {
+
+		artistaService.incluir(artista);
+
+		assertEquals(artista, artistaService.obter(ID));
+	}
+
+	@Test
+	void exclusao() {
+		inclusao();
+
+		artistaService.excluir(ID);
+
+        try {
+            assertNull(artistaService.obter(NOME));
+            fail();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+	}
+
+	@Test
+	void recuperacaoTotal() {
+		inclusao();
+
+		assertTrue(artistaService.obterLista().contains(artista));
+	}
+}
